0b30ff077c1da55f5fc93b5db1707d121d5b7fb4
amagdi <ahmed.magdi.hosny@gmail.com>
2023-12-27 16:48:41 +0200

    Initial Project Add

---
+++ src/test/java/springFramework/PetClinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package springFramework.PetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
